c636b11969e1ef6fa3512394e6c8ed8fd0b84757
cadfrunze <marius1890@yahoo.com>
2023-08-19 02:10:31 +0300

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from stock_trading import preturi
 suma_lista: list = [float(num) for num in preturi.values()]
 yesterday_price: float = float(round(suma_lista[0], 2))
 alaltaieri_price: float = float(round(suma_lista[-1], 2))
+print(news)
 print(yesterday_price, alaltaieri_price)
 
 if yesterday_price > alaltaieri_price:
